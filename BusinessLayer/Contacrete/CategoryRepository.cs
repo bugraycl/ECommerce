8ec496dde49bea9323843d30336746cce0313e40
Buğra YÜCEL <bugrayuucelx@gmail.cm>
2024-06-20 18:25:59 +0300

    interfaces and repositories created for business logic layer

---
+++ BusinessLayer/Contacrete/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Contacrete
+{
+    public class CategoryRepository:GenericRepository<Category>
+    {
+
+    }
+}
